73d835d1896593266655cdd6776c801eb9b9e714
Z <nyitraizsolt1993>
2020-10-14 20:13:58 +0300

    next,prev,quit buttons, some sounds

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import questions from '../questions.json'
 import Container from 'react-bootstrap/Container'
 import Helmet from 'react-helmet'
@@ -9,6 +9,8 @@ import { BsSquareHalf } from 'react-icons/bs';
 import { FaRegLightbulb } from 'react-icons/fa';
 import { BsFillClockFill } from "react-icons/bs";
 import isEmpty from '../isEmpty'
+import Correct from '../assets/sounds/correct.mp3'
+import Wrong from '../assets/sounds/wrong.mp3'
 
 
 class Quiz extends React.Component{
@@ -25,8 +27,8 @@ class Quiz extends React.Component{
             numberOfAnsweredQuestions:0,
             currentQuestionIndex:0,
             score:0,
-            correncAnswers:0,
-            wrongAnswer:0,
+            correctAnswers:0,
+            wrongAnswers:0,
             hints:5,
             fiftyFifty:2,
             usedFiftyFifty:false,
@@ -51,19 +53,102 @@ class Quiz extends React.Component{
                 currentQuestion: currentQuestion,
                 nextQuestion: nextQuestion,
                 previousQuestion:previousQuestion,
+                numberOfQuestions:questions.length,
                 answer:answer
             })
         }
     }
 
+    handleOptionClick=(e)=>{
+        if (e.target.innerHTML.toLowerCase()==this.state.answer.toLowerCase()){
+            setTimeout(()=>{
+                document.getElementById('correct-sound').play()
+            },100)
+            this.correctAnswer()
+        }else{
+            this.wrongAnswer()
+            setTimeout(()=>{
+                document.getElementById('wrong-sound').play()
+            },100)
+        }
+    }
+
+    handleButtonClick=(e)=>{
+        switch(e.target.id){
+            case 'next-button':
+                this.handleNextButtonClick();
+                break;
+            case 'previous-button':
+                this.handlePreviousButtonClick();
+                break;
+            case 'quit-button':
+                this.handleQuitButtonClick();
+                break;
+            default:
+                break;
+        }
+    }
+
+    handleNextButtonClick=()=>{
+        if(this.state.nextQuestion !== undefined){
+            this.setState(prevState=>({
+                currentQuestionIndex: prevState.currentQuestionIndex+1
+            }),()=>{
+                this.displayQuestions(this.state.state,this.state.currentQuestion,this.state.nextQuestion,this.state.previousQuestion)
+            })
+        }
+    }
+
+    handleQuitButtonClick=()=>{
+        if(window.confirm('Are you sure?')){
+            this.props.history.push('/')
+        }
+    }
+
+    handlePreviousButtonClick=()=>{
+        if(this.state.previousQuestion !== undefined){
+            this.setState(prevState=>({
+                currentQuestionIndex: prevState.currentQuestionIndex-1
+            }),()=>{
+                this.displayQuestions(this.state.state,this.state.currentQuestion,this.state.nextQuestion,this.state.previousQuestion)
+            })
+        }
+    }
+
+    correctAnswer=()=>{
+        this.setState(prevState=>({
+            score:prevState+1,
+            correctAnswers:prevState.correctAnswers+1,
+            currentQuestionIndex:prevState.currentQuestionIndex+1,
+            numberOfAnsweredQuestions:prevState.numberOfAnsweredQuestions+1
+        }),()=>{
+            this.displayQuestions(this.state.questions,this.state.currentQuestion,this.state.nextQuestion,this.state.previousQuestion)
+        })
+    }
+
+    wrongAnswer=()=>{
+        this.setState(prevState=>({
+            wrongAnswers:prevState.wrongAnswers + 1,
+            currentQuestionIndex:prevState.currentQuestionIndex+1,
+            numberOfAnsweredQuestions:prevState.numberOfAnsweredQuestions+1,
+        }),()=>{
+            this.displayQuestions(this.state.questions,this.state.currentQuestion,this.state.nextQuestion,this.state.previousQuestion)
+        })
+    }
+
     render(){
-        const {currentQuestion}=this.state
+        const {currentQuestion, currentQuestionIndex,numberOfQuestions}=this.state
 
         return(
             <Container  className='quiz-box justify-content-center'>
                 <Helmet><title>Quiz Page</title></Helmet>
+                <Fragment>
+                <audio id='correct-sound' src={Correct}></audio>
+                <audio id='wrong-sound' src={Wrong}></audio>
+                </Fragment>
+
                 <div>
-                    <div className='display-4 text-center'>{currentQuestion.question}</div>
+                    <div className='h2 text-center'>{currentQuestion.question}</div>
                     {/* helpers */}
                     <Row>
                     <Col className=''>
@@ -74,7 +159,7 @@ class Quiz extends React.Component{
 
                     {/* nr question and time */}
                     <Row className='mb-2'>
-                        <Col className ='float-left'><span className='float-left'>1 of 15</span></Col>
+                    <Col className ='float-left'><span className='float-left'>{currentQuestionIndex+1} of {numberOfQuestions}</span></Col>
                         <Col className ='float-right'>
                             <span className='float-right'>15:00</span>
                             <BsFillClockFill className='float-right m-1'/>
@@ -83,20 +168,20 @@ class Quiz extends React.Component{
 
                     <div className='justify-content-center'>
                         <Row className='m-3'>
-                            <Col className='quiz-button'>{currentQuestion.optionA}</Col>
-                            <Col className='quiz-button'>{currentQuestion.optionB}</Col>
+                            <Col onClick={this.handleOptionClick} className='quiz-button'>{currentQuestion.optionA}</Col>
+                            <Col onClick={this.handleOptionClick} className='quiz-button'>{currentQuestion.optionB}</Col>
                         </Row>
                         <Row className='m-3'>
-                            <Col className='quiz-button'>{currentQuestion.optionC}</Col>
-                            <Col className='quiz-button'>{currentQuestion.optionD}</Col>
+                            <Col onClick={this.handleOptionClick} className='quiz-button'>{currentQuestion.optionC}</Col>
+                            <Col onClick={this.handleOptionClick} className='quiz-button'>{currentQuestion.optionD}</Col>
                         </Row>
                     </div>
                 </div>
 
                 <div className=''>
-                    <Button variant='primary' className='m-2'>Previous</Button>
-                    <Button variant='success' className='m-2'>Next</Button>
-                    <Button variant='danger' className='m-2'>Quit</Button>
+                    <Button id='previous-button' variant='primary' onClick={this.handleButtonClick} className='m-2'>Previous</Button>
+                    <Button id='next-button' variant='success' onClick={this.handleButtonClick} className='m-2'>Next</Button>
+                    <Button id='quit-button' variant='danger' onClick={this.handleButtonClick} className='m-2'>Quit</Button>
                 </div>
             </Container>
         )
--- src/components/QuizInstructions.js
+++ src/components/QuizInstructions.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React ,{Fragment} from 'react'
 import {Link} from 'react-router-dom'
 import {Helmet} from 'react-helmet'
 import Container from 'react-bootstrap/Container'
 import Row from'react-bootstrap/Row'
 import { FiChevronsRight } from "react-icons/fi";
 import { BsFillClockFill } from "react-icons/bs";
+import Correct from '../assets/sounds/correct.mp3'
+import Wrong from '../assets/sounds/wrong.mp3'
+import {BsPlayFill} from 'react-icons/bs'
+
 
 
 const QuizInstructions=()=>(
     <Container className='quiz-container border mt-5 '>
-        
         <Helmet><title>Quiz Instructions</title></Helmet>
+        <Fragment>
+            <audio id='correct-sound' src={Correct}></audio>
+            <audio id='wrong-sound' src={Wrong}></audio>
+        </Fragment>
         <Row className='display-4 justify-content-center'>
         How to Play the Game 
         </Row>
@@ -25,6 +32,15 @@ const QuizInstructions=()=>(
                  <div className='border-bottom m-4'>Hint: removes 1 wrong answer (total hints:5)</div>
             </div>
         </div>
+        <div className='instruction-detail border-bottom m-4'><FiChevronsRight/>
+            If your answer is <span className='text-success'>Correct</span> you will hear: 
+            <span onClick={()=>document.getElementById('correct-sound').play()}><BsPlayFill className='correct-icon'/></span>
+        </div>
+        <div className='instruction-detail border-bottom m-4'><FiChevronsRight/> 
+            If your answer is <span className='text-danger'>Wrong</span> you will hear: 
+            <span onClick={()=>document.getElementById('wrong-sound').play()}><BsPlayFill className='wrong-icon'/></span>
+        </div>
+
         <div className='text-info m-4'><BsFillClockFill/>The timer starts as soon as the game loads</div>
         <div>
             <span className='float-left'><Link className='text-danger' to='/'>Take me back</Link></span>
